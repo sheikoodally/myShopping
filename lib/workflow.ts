1706ab9507a63974a38657569c6f182f62e58174
sheik oodally <sheikoodally@hotmail.com>
2025-03-04 15:20:57 -0500

    latest

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,4 +1,5 @@
 import {Client as WorkflowClient } from '@upstash/workflow';
+import { Client as QstashClient, resend } from "@upstash/qstash";
 import config from './config';
 
 
@@ -6,3 +7,22 @@ export const workflowClient = new WorkflowClient({
   baseUrl:config.env.upstash.qstashUrl,
   token:config.env.upstash.qstashToken,
 })
+
+const qstashClient = new QstashClient({ token: config.env.upstash.qstashToken });
+
+export const sendEmail = async ({ email, subject, message } : { email: string; subject: string; message: string }) =>{
+
+  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resend }),
+    },
+    body: {
+      from: "MyShopping <hello.sheikoodally.online>",
+      to: [email],
+      subject: subject,
+      html: message,
+    },
+  });
+
+}
